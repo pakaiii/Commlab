a95704d75f7480875a96422741623ce6194444de
pakaiii <pv2286@nyu.edu>
2026-04-27 18:30:02 +0800

    sum
    sum

--- narrativefinal/script.js
+++ narrativefinal/script.js
@@ -1,10 +1,28 @@
 
+const letters = document.querySelectorAll("#title span");
 
 window.addEventListener("scroll", function(){
 
     console.log("scroll test");
 
+    const scrollTop = window.scrollY;
+    const maxScroll = document.body.scrollHeight - window.innerHeight;
+
+    const progress = 1 - (scrollTop / maxScroll);
+
+    const lettersToFill = Math.floor(progress * letters.length);
+
+    letters.forEach((letter, index) => {
+        if (index < lettersToFill) {
+            letter.style.color = "orange";
+        } else {
+            letter.style.color = "black";
+        }
+    });
 });
 
+window.onload = function () {
+  window.scrollTo(0, document.body.scrollHeight);
+};
 
 
